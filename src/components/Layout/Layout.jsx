153204511fa69b479ecc35777d38c1234610794f
Adrian Lacheta <adiczq@gmail.com>
2023-07-26 19:34:46 +0200

    Create Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React, { Suspense } from 'react';
+import Header from 'components/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Loader from 'components/Loader/Loader';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
+};
+
+export default Layout;
